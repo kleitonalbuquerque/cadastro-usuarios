4ba4bda8d12c1f5cfa2263d6191a0e26ab7dd7a3
kleitonalbuquerque <kleiton2102@gmail.com>
2022-11-06 13:44:19 -0300

    Componente Logo

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,4 +1,9 @@
 /* eslint-disable import/no-anonymous-default-export */
 import "./Logo.css";
+import logo from '../../assets/imgs/logo.png'
 
-export default (props) => <aside className="logo">Logo</aside>;
+export default (props) => <aside className="logo">
+  <a className="logo" href="/">
+    <img src={logo} alt="logo" />
+  </a>
+</aside>;
